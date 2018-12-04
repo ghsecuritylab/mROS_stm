1ecc0cb01da1a7f2d6d758f69f2893eb18f627e2
takasehideki <takase@i.kyoto-u.ac.jp>
2018-12-04 19:08:06 +0900

    copy project from adafruit_tft_sample

---
+++ workspace/lwip_http_server_netconn/lwip_http_server_netconn.h
@@ -0,0 +1,40 @@
+/*
+ *  $Id: lwip_http_server_netconn.h
+ */
+
+/*
+ *  ターゲット依存の定義
+ */
+#include "target_test.h"
+
+/*
+ *  各タスクの優先度の定義
+ */
+
+#define MAIN_PRIORITY	5		/* メインタスクの優先度 */
+#define HIGH_PRIORITY	9
+#define MID_PRIORITY	10
+#define LOW_PRIORITY	11
+
+/*
+ *  ターゲットに依存する可能性のある定数の定義
+ */
+
+#ifndef TASK_PORTID
+#define	TASK_PORTID		1			/* 文字入力するシリアルポートID */
+#endif /* TASK_PORTID */
+
+#ifndef STACK_SIZE
+#define	STACK_SIZE		4096		/* タスクのスタックサイズ */
+#endif /* STACK_SIZE */
+
+/*
+ *  関数のプロトタイプ宣言
+ */
+#ifndef TOPPERS_MACRO_ONLY
+
+extern void	led_task(intptr_t exinf);
+extern void	tft_task(intptr_t exinf);
+extern void	main_task(intptr_t exinf);
+
+#endif /* TOPPERS_MACRO_ONLY */
